22020e995aff650b0dbf0c7f531b3d7a033016b4
SevenMon <57867782@qq.com>
2019-08-14 17:06:11 +0800

    sdf

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,18 @@ class User extends Authenticatable
     public function statuses(){
         return $this->hasMany(Status::class);
     }
+
+    public function feed(){
+        return $this->statuses()->orderBy('created_at','desc');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
 }
--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,3 +1,5 @@
+
+
 <li class="media mt-4 mb-4">
     <a href="{{ route('users.show', $user->id )}}">
         <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
@@ -6,4 +8,12 @@
         <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
         {{ $status->content }}
     </div>
+
+    @can('destroy', $status)
+        <form action="{{ route('statuses.destroy', $status->id) }}" method="POST">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-sm btn-danger">删除</button>
+        </form>
+    @endcan
 </li>
